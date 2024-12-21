7526bcdda857d82528509a8ce7e63bbafd5919a1
Muji1412 <migmaknoob@gmail.com>
2024-12-21 12:55:47 +0900

    [Silver V] Title: 영화감독 숌, Time: 340 ms, Memory: 89152 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int count = 0;
+        int i = 0;
+        while (count<n) {
+            i++;
+            if (String.valueOf(i).contains("666")) {
+                count++;
+                if (count == n) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
