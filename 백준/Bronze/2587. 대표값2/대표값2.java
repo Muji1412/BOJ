1617c0404f11eb8a32d20991ef4cef4651620410
Muji1412 <migmaknoob@gmail.com>
2025-01-05 14:08:59 +0900

    [Bronze II] Title: 대표값2, Time: 172 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+
+    }
+}
