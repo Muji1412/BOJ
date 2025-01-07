49630e1f3ea425d3316f2273832af11eda6d2caf
Muji1412 <migmaknoob@gmail.com>
2025-01-07 15:26:08 +0900

    [Silver V] Title: 숫자 카드, Time: 2076 ms, Memory: 355812 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,9 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         // 상근이 카드를 해시맵으로 만들어줘야함
 
         int n = scanner.nextInt();
@@ -15,26 +19,28 @@ public class Main {
 
         // 대조카드 어레이
         int m = scanner.nextInt();
-        int[] arr = new int[m];
-        for (int i = 0; i < m; i++) {
-            int a = scanner.nextInt();
-            arr[i] = a;
-            if (!map.containsKey(a)) {
-                map.put(a,0);
-            }
-        }
+
+        // 이 부분이 통째로 필요가 없음
+//        int[] arr = new int[m];
+//        for (int i = 0; i < m; i++) {
+//            int a = scanner.nextInt();
+//            arr[i] = a;
+//            if (!map.containsKey(a)) {
+//                map.put(a,0);
+//            }
+//        }
 
 
 
         StringBuilder sb = new StringBuilder();
 
-        for (int value : arr) {
-            sb.append(map.get(value)).append(" ");
+        for (int i = 0; i < m; i++) {
+            // 이 부분을 getOrDefault(value,0) 으로 바꿔주면, 일치하는 경우에는 키값을 가져오고 아닌경우에는 0을 디폴트로 출력
+            int value = scanner.nextInt();
+            sb.append(map.getOrDefault(value,0)).append(" ");
         }
 
-
         System.out.println(sb);
-
 
     }
 }
