e3c71cd33094733eb8cfa09b469608c3deb8f6ad
Muji1412 <migmaknoob@gmail.com>
2025-01-16 16:21:37 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,30 +1,15 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int[] nums = new int[10];
-        int count = 0;
-        boolean isDup;
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = scanner.nextInt() % 42;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            set.add(sc.nextInt() % 42);
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            isDup = false;
-
-            for (int j = 0; j < i; j++) {
-                if (nums[i] == nums[j]) {
-                    isDup = true;
-                    break;
-                }
-            }
-            if (!isDup) {
-                count++;
-            }
-        }
-        System.out.println(count);
+        System.out.println(set.size());
     }
 }
