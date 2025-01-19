89e9306e641be5e9a13d4bfb09e8b5c77e57f607
Muji1412 <migmaknoob@gmail.com>
2025-01-19 20:37:30 +0900

    [Bronze II] Title: 대표값, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int n = 10;
+        int sum = 0;
+        int highest = 0;
+        int value = 0;
+        while (n-- > 0) {
+            int a = sc.nextInt();
+            sum += a;
+            map.put(a, map.getOrDefault(a, 0) + 1);
+            if (map.get(a) > highest) {
+                highest = map.get(a);
+                value = a;
+            }
+        }
+
+        System.out.println(sum / 10);
+        System.out.println(value);
+
+
+    }
+}
