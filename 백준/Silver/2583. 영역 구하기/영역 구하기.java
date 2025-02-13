3f676a4ffb83bd0a32cc13fc19a3a5bd2ea96e69
Muji1412 <migmaknoob@gmail.com>
2025-02-13 11:27:29 +0900

    [Silver I] Title: 영역 구하기, Time: 208 ms, Memory: 19248 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -17,7 +17,7 @@ public class Main {
         int k = sc.nextInt();
         arr = new int[n][m]; // 빈 곳은 기본값인 0으로 냅두고, 채워야할 부분은 1로 바꿔넣기
         visited = new boolean[n][m];
-        while (k-- > 0) {
+        while (k-- > 0) { // k번만큼 모눈종이에 색칠해줌
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
             int x2 = sc.nextInt();
@@ -25,22 +25,23 @@ public class Main {
             drawing(x1, y1, x2, y2);
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>(); // 저장용 리스트
+
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
+                if (arr[i][j] == 0) { // 전부 탐색, 0으로 빈곳이 남아있으면 dfs로 체크
                     countTotalArea++;
                     dfs(i, j);
-                    list.add(countCurrentArea);
-                    countCurrentArea = 0;
+                    list.add(countCurrentArea); // dfs에서 현재 체크중인 땅을 세주니까 그 값을 받아서 리스트에 삽입
+                    countCurrentArea = 0; // 다음 dfs에서 또 써야하니까 초기화
                 }
             }
         }
 
 
         System.out.println(countTotalArea);
-        Collections.sort(list);
+        Collections.sort(list); // 문제에서 오름차순 정렬 요구
         for (int val : list) {
             System.out.print(val);
             System.out.print(" ");
@@ -48,14 +49,16 @@ public class Main {
 
 
     }
-    public static void drawing(int x1, int y1, int x2, int y2) {
+    public static void drawing(int x1, int y1, int x2, int y2) { // 종이 칠해주는 메서드, 단, xy값 거꾸로 집어넣어야 정상적으로 작동
+        // 수학에서의 x는 가로, y는 세로로 사용
+        // 자바에서 2차원 배열은 int[n][m] 기준으로 n을 세로로, m을 가로로 사용하므로 거꾸로 넣어야 작동
         for (int i = y1; i < y2; i++) {
             for (int j = x1; j < x2; j++) {
                 arr[i][j] = 1;
             }
         }
     }
-    public static boolean checkRange(int i, int j) {
+    public static boolean checkRange(int i, int j) { // 범위체크 메서드, arr 밖으로 안나가게
         if (i < 0 || i >= n || j < 0 || j >= m) {
             return false;
         }
@@ -71,12 +74,15 @@ public class Main {
         arr[i][j] = 1;
         countCurrentArea++;
 
-        for (int k = 0; k < 4; k++) {
+        for (int k = 0; k < 4; k++) { // 위에 적어놓은 dx,dy 4방향에 대해서 탐색
             int nextX = i + dx[k];
             int nextY = j + dy[k];
             if (checkRange(nextX, nextY)&&!visited[nextX][nextY] && arr[nextX][nextY] == 0) {
+                // arr 안에 있는지 + 이미 탐색한 곳이 아닌지 + 탐색할곳이 0인지에 대해 체크
                 dfs(nextX, nextY);
+                // 재귀
             }
         }
     }
 }
+
